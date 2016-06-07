f32226486eb2c8fcbbd7bd0c6f2fc0482c6a6d62
hao.tran <hao.tran@seldatinc.com>
2016-06-07 13:41:50 +0700

    Use yandex to translate

--- src/TranslateAbstract.php
+++ src/TranslateAbstract.php
@@ -3,26 +3,33 @@ namespace TranslateApi;
 
 abstract class TranslateAbstract
 {
-    const API_KEY = 'key';
-    const API_SOURCE = 'source';
-    const API_TARGET = 'target';
-    const API_TEXT = 'text';
-    const API_RESPONSE_TYPE = 'responseType';
-
-    const RESPONSE_TYPE_JSON = 'json';
-    const RESPONSE_TYPE_XML = 'xml';
+    /**
+     * Key to connect with provider
+     * @var string
+     */
+    private $apiKey = '';
 
     /**
      * @var array Container all query string
      */
-    public $options = [];
+    private $options = [];
+
+//    const API_KEY = 'key';
+//    const API_SOURCE = 'source';
+//    const API_TARGET = 'target';
+//    const API_TEXT = 'text';
+//    const API_RESPONSE_TYPE = 'responseType';
+//
+//    const RESPONSE_TYPE_JSON = 'json';
+//    const RESPONSE_TYPE_XML = 'xml';
+
 
     /**
      * Constructor
-     * @param array $opts
+     * @param $apiKey
      */
-    public function __construct($opts = array()) {
-        $this->setOptions($opts);
+    public function __construct($apiKey) {
+        $this->setApiKey($apiKey);
     }
 
     /**
@@ -31,7 +38,7 @@ abstract class TranslateAbstract
      * @return $this
      */
     public function setApiKey($apiKey) {
-        $this->setOption(self::API_KEY, $apiKey);
+        $this->$apiKey = $apiKey;
 
         return $this;
     }
@@ -40,112 +47,147 @@ abstract class TranslateAbstract
      * @return string
      */
     public function getApiKey() {
-        return $this->getOption(self::API_KEY);
+        return $this->apiKey;
     }
 
+//    /**
+//     * Set source translate
+//     * @param string $source
+//     * @return $this
+//     */
+//    public function setSource($source) {
+//        $this->setOption(self::API_SOURCE, $source);
+//
+//        return $this;
+//    }
+//    /**
+//     * Get source translate
+//     * @return string
+//     */
+//    public function getSource() {
+//        return $this->getOption(self::API_SOURCE);
+//    }
+
+//    /**
+//     * Set target translate
+//     * @param string $target
+//     * @return $this
+//     */
+//    public function setTarget($target) {
+//        $this->setOption(self::API_TARGET, $target);
+//
+//        return $this;
+//    }
+//    /**
+//     * Get target translate
+//     * @return string
+//     */
+//    public function getTarget() {
+//        return $this->getOption(self::API_TARGET);
+//    }
+
+//    /**
+//     * Set text translate
+//     * @param string $text
+//     * @return $this
+//     */
+//    public function setText($text) {
+//        $this->setOption(self::API_TEXT, $text);
+//
+//        return $this;
+//    }
+//    /**
+//     * Set text translate
+//     * @return string
+//     */
+//    public function getText() {
+//        return $this->getOption(self::API_TEXT);
+//    }
+
+//    /**
+//     * Set response type
+//     * @param string $responseType
+//     * @return $this
+//     */
+//    public function setResponseType($responseType) {
+//        $this->setOption(self::API_RESPONSE_TYPE, $responseType);
+//
+//        return $this;
+//    }
+//
+//    /**
+//     * Get response type
+//     * @return string
+//     */
+//    public function getResponseType() {
+//        $responseType = $this->getOption(self::API_RESPONSE_TYPE);
+//        if(!$responseType) {
+//            $responseType = self::RESPONSE_TYPE_JSON;
+//        }
+//
+//        return $responseType;
+//    }
+
     /**
-     * Set source translate
-     * @param string $source
+     * Set option
+     * @param $key
+     * @param $value
      * @return $this
      */
-    public function setSource($source) {
-        $this->setOption(self::API_SOURCE, $source);
-
-        return $this;
-    }
-    /**
-     * Get source translate
-     * @return string
-     */
-    public function getSource() {
-        return $this->getOption(self::API_SOURCE);
-    }
-
-    /**
-     * Set target translate
-     * @param string $target
-     * @return $this
-     */
-    public function setTarget($target) {
-        $this->setOption(self::API_TARGET, $target);
-
-        return $this;
-    }
-    /**
-     * Get target translate
-     * @return string
-     */
-    public function getTarget() {
-        return $this->getOption(self::API_TARGET);
-    }
-
-    /**
-     * Set text translate
-     * @param string $text
-     * @return $this
-     */
-    public function setText($text) {
-        $this->setOption(self::API_TEXT, $text);
-
-        return $this;
-    }
-    /**
-     * Set text translate
-     * @return string
-     */
-    public function getText() {
-        return $this->getOption(self::API_TEXT);
-    }
-
-    /**
-     * Set response type
-     * @param string $responseType
-     * @return $this
-     */
-    public function setResponseType($responseType) {
-        $this->setOption(self::API_RESPONSE_TYPE, $responseType);
-
-        return $this;
-    }
-
-    /**
-     * Get response type
-     * @return string
-     */
-    public function getResponseType() {
-        $responseType = $this->getOption(self::API_RESPONSE_TYPE);
-        if(!$responseType) {
-            $responseType = self::RESPONSE_TYPE_JSON;
-        }
-
-        return $responseType;
-    }
-
     public function setOption($key, $value) {
         $this->options[$key] = $value;
 
         return $this;
     }
 
+    /**
+     * Get option
+     * @param $key
+     * @return mixed
+     */
     public function getOption($key) {
         return $this->options[$key];
     }
 
+    /**
+     * Set options
+     * @param array $opts
+     * @return $this
+     */
     public function setOptions($opts = array()) {
         foreach($opts as $key => $value) {
             $this->setOption($key, $value);
+//            switch($key) {
+//                case self::API_KEY:
+//                case self::API_SOURCE:
+//                case self::API_TARGET:
+//                case self::API_TEXT:
+//                case self::API_RESPONSE_TYPE:
+//                    break;
+//                default:
+//
+//                    break;
+//            }
         }
 
         return $this;
     }
 
+    /**
+     * Get options
+     * @return array
+     */
     public function getOptions() {
         return $this->options;
     }
 
     /**
-     * Create url to translate
-     * @return string
+     * Translate
+     * @param string $text
+     * @param string $source
+     * @param string $target
+     * @param array $options
+     * @return mixed
      */
-    abstract public function createLinkTranslate();
+    abstract public function translate($text, $source, $target, $options = []);
 }
--- src/TranslateYandex.php
+++ src/TranslateYandex.php
@@ -10,18 +10,47 @@ class TranslateYandex extends TranslateAbstract
     const LINK_TRANSLATE_JSON = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
 
     /**
-     * Create url to translate
+     * Create query string
      * @return string
      */
-    public function createLinkTranslate()
+    public function createQueryString($text, $source, $target, $options)
     {
-        switch($this->getResponseType()) {
-            case self::RESPONSE_TYPE_JSON:
-                break;
-            case self::RESPONSE_TYPE_XML:
-                break;
-            default:
-                break;
+        $opts = array_merge($this->getOptions(), $options);
+        $textOpt = ['text' => $text];
+        $langOpt = ['lang' => implode('-', [$source, $target])];
+        $apiKey = ['key' => $this->getApiKey()];
+
+        $queryStringData = array_merge($opts, $textOpt, $langOpt, $apiKey);
+
+        return http_build_query(array_filter($queryStringData));
+    }
+
+    /**
+     * Translate
+     * @param string $text
+     * @param string $source
+     * @param string $target
+     * @param array $options
+     * @return mixed
+     */
+    public function translate($text, $source, $target, $options = [])
+    {
+        $queryString = $this->createQueryString($text, $source, $target, $options);
+
+        // create curl resource
+        $ch = curl_init();
+
+        // set url
+        curl_setopt($ch, CURLOPT_URL, self::LINK_TRANSLATE_JSON);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $queryString);
+
+        $output = curl_exec($ch);
+        if($output === false) {
+            return new ResponseObject(false, [], curl_error($ch));
         }
+
+        return new ResponseObject(true, $output, null);
+
     }
 }
